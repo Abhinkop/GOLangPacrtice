17b6e5a21b7189200108ccb71c64f9f3f4c4d8f4
Abhinkop <abhinjose@live.com>
2018-10-09 00:50:02 +0530

    The App now currently pulls and filters news from "the washington Post" and "the Daily Mail UK" and concurently gets them

--- news.go
+++ news.go
@@ -1,7 +1,6 @@
 package main
 
-// An App that currently filters out only Soccer news fron the "Washington Post"
-// Adding somthing still in learning
+// An App that currently filters out only Soccer,the real "Football" news fron the "Washington Post","DailyMail UK"
 import (
 	"encoding/xml"
 	"fmt"
@@ -36,18 +35,15 @@ type SingleNews struct {
 }
 
 func index_handler(w http.ResponseWriter, r *http.Request) {
-	//p := SingleNews{"Title","Location"}
-	football_news := make(map[string]NewsMap)
-	x1 := []string{"SOCCER", "FOOTBALL"}
-	GetNews(football_news, x1)
-	//fmt.Println(football_news)
-	t, err := template.ParseFiles("BasicTemplate.html")
+	football_news := make(map[string]NewsMap) // A map to holf the output refactor this to use SingleNews Slice
+	footbalKeywords := []string{"SOCCER", "FOOTBALL"}
+	GetNews(football_news, footbalKeywords)
+	template, err := template.ParseFiles("BasicTemplate.html") // template to display the news. Beautify the HTML
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(-1)
 	}
-	//fmt.Println(p)
-	err = t.Execute(w, football_news)
+	err = template.Execute(w, football_news)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(-1)
@@ -56,21 +52,17 @@ func index_handler(w http.ResponseWriter, r *http.Request) {
 
 func GetNewsRoutine(locs string, c chan SingleNews, Keyword []string, trackingChannel chan string) {
 	defer fmt.Println("closed", locs)
-	//defer wg.Done()
 	fmt.Println("launched", locs)
-	var n News
+	var n News // used to extract all the news
 	response, _ := http.Get(locs)
 	bytes, _ := ioutil.ReadAll(response.Body)
 	xml.Unmarshal(bytes, &n)
 	response.Body.Close()
-	//i := 0
 	for index, _ := range n.Titles {
-		//fmt.Println(n.Titles[index])
-		upper_title := strings.ToUpper(n.Keywords[index])
-		for _, key1 := range Keyword {
-			if strings.Contains(upper_title, key1) {
-				fmt.Println(n.Titles[index])
-				//i = i + 1
+		upper_Keyword := strings.ToUpper(n.Keywords[index])
+		for _, key := range Keyword {
+			if strings.Contains(upper_Keyword, key) {
+				// Send the news across the channel
 				c <- SingleNews{
 					n.Titles[index],
 					n.Locations[index],
@@ -78,65 +70,79 @@ func GetNewsRoutine(locs string, c chan SingleNews, Keyword []string, trackingCh
 			}
 		}
 	}
+	// Inform The use of the channel is done from the sender and hence the reciever can close it.
 	trackingChannel <- locs
 }
 
-func GetLocs(in_siteName string, c chan string) {
-	//var Locations []string
-	defer wg.Done()
-	var url string
-	if in_siteName == "Washington Post" {
-		url = "https://www.washingtonpost.com/news-sitemap-index.xml"
-	}
-	if in_siteName == "Daily Mail UK" {
-		url = "https://www.dailymail.co.uk/newssitemap.xml"
-	}
-	var s SiteMapIndex
+func GetLocs(in_sitemapURL string, c chan string, trackingChannel chan string) {
+	var s SiteMapIndex // Used to extract the locations of the news
 	response, _ :=
-		http.Get(url)
+		http.Get(in_sitemapURL)
 	bytes, _ := ioutil.ReadAll(response.Body)
 	xml.Unmarshal(bytes, &s)
 	response.Body.Close()
 	for _, locs := range s.Locations {
-		//Locations = append(Locations, locs)
+		//Send the Locations across the channel
 		c <- locs
 	}
-	//return Locations
+	// Inform The use of the channel is done from the sender and hence the reciever can close it.
+	trackingChannel <- in_sitemapURL
 }
 
 func GetNews(out_map map[string]NewsMap, Keyword []string) {
 	fmt.Printf("Getting %s News Started\n", Keyword)
-
-	//var s SiteMapIndex
-	//news_map := make(map[string]NewsMap)
-	xx := make(chan string, 30)
-	wg.Add(1)
-	//go GetLocs("Washington Post", xx)
-	go GetLocs("Daily Mail UK", xx)
-	wg.Wait()
-	close(xx)
-	trackingMap := make(map[string]bool)
-	channelForTracking := make(chan string)
-	q := make(chan SingleNews, 2)
-	for locs := range xx {
-		fmt.Println(locs)
-		//wg.Add(1)
-		trackingMap[locs] = true
-		go GetNewsRoutine(locs, q, Keyword, channelForTracking)
+	// Used to store the root URL of the sitemaps
+	SiteMapRootURL := []string{
+		"https://www.washingtonpost.com/news-sitemap-index.xml",
+		"https://www.dailymail.co.uk/newssitemap.xml"}
+	locsTrackingMap := make(map[string]bool)       // used to track if the go routine has completed exection.
+	channelForTrackingGetLocs := make(chan string) // channel used by the goroutine to
+	//communicate back that i has completed execution.
+	LocsChannel := make(chan string, 30) // Channel to send Location of the news
+	var LocationSlice []string
+	for _, rootPath := range SiteMapRootURL {
+		locsTrackingMap[rootPath] = true
+		go GetLocs(rootPath, LocsChannel, channelForTrackingGetLocs)
 	}
-	//fmt.Println(ll)
-	// response, _ :=
-	// 	http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
-	// bytes, _ := ioutil.ReadAll(response.Body)
-	// xml.Unmarshal(bytes, &s)
-	// for _, locs := range s.Locations {
-	// 	wg.Add(1)
-	// 	go GetNewsRoutine(locs, q, Keyword)
-	// }
 	var breakflag bool = false
 	for {
 		select {
-		case news := <-q:
+		case locs := <-LocsChannel:
+			{
+				LocationSlice = append(LocationSlice, locs)
+			}
+		case rootPath := <-channelForTrackingGetLocs:
+			{
+				locsTrackingMap[rootPath] = false
+				breakflag = true
+				// check if all the go routines are completed then set flag to break out of the infinte for loop
+				for _, val := range locsTrackingMap {
+					if val {
+						breakflag = false
+						break
+					}
+				}
+			}
+		}
+		if breakflag == true {
+			// break out of the infinte for loop
+			break
+		}
+	}
+	close(LocsChannel)
+	close(channelForTrackingGetLocs)
+	trackingMap := make(map[string]bool)    // used to track if the go routine has completed exection.
+	channelForTracking := make(chan string) // channel used by the goroutine to
+	//communicate back that i has completed execution.
+	NewsChannel := make(chan SingleNews, 2) // Channel to send the news
+	for _, locs := range LocationSlice {
+		trackingMap[locs] = true
+		go GetNewsRoutine(locs, NewsChannel, Keyword, channelForTracking)
+	}
+	breakflag = false
+	for {
+		select {
+		case news := <-NewsChannel:
 			{
 				out_map[news.Title] = NewsMap{
 					news.Keyword,
@@ -146,8 +152,8 @@ func GetNews(out_map map[string]NewsMap, Keyword []string) {
 			{
 				trackingMap[loc] = false
 				breakflag = true
-				for loc, val := range trackingMap {
-					fmt.Println(loc, val)
+				// check if all the go routines are completed then set flag to break out of the infinte for loop
+				for _, val := range trackingMap {
 					if val {
 						breakflag = false
 						break
@@ -156,22 +162,16 @@ func GetNews(out_map map[string]NewsMap, Keyword []string) {
 			}
 		}
 		if breakflag == true {
+			// break out of the infinte for loop
 			break
 		}
 	}
-	// for n := range q {
-
-	// 	out_map[n.Title] = NewsMap{
-	// 		n.Keyword,
-	// 		n.Loc}
-	// }
-	//fmt.Println(out_map)
-	//response.Body.Close()
+	close(NewsChannel)
+	close(channelForTracking)
 	fmt.Printf("Getting %s News Ended\n", Keyword)
 }
 
 func main() {
-	http.HandleFunc("/a", index_handler)
+	http.HandleFunc("/News", index_handler)
 	http.ListenAndServe(":8000", nil)
-
 }
